9e304b92fdcaa26a512dd8c2df69051a7d93930f
Balvant <Devvrat1003>
2025-01-21 18:37:05 +0530

    changed API URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
     const getRespone = async () => {
-        let res = await fetch("http://127.0.0.1:8000/api/v1/dummy/posts");
+        let res = await fetch("https://wsgibm.onrender.com/api/v1/dummy/posts");
         let data = await res.json();
 
         console.log(data, "data");
